8b73553e4ab4f3f423491053a089aa02dd59981b
zdenek.castoral <zdenek.castoral@inventi.cz>
2025-08-31 13:44:13 +0200

    Init of githubgitlab service.

---
+++ github-gitlab-service/src/main/java/eu/profinit/githubgitlabservice/GithubGitlabServiceApplication.java
@@ -0,0 +1,13 @@
+package eu.profinit.githubgitlabservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubGitlabServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubGitlabServiceApplication.class, args);
+	}
+
+}
---
+++ github-gitlab-service/src/test/java/eu/profinit/githubgitlabservice/GithubGitlabServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.profinit.githubgitlabservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubGitlabServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
